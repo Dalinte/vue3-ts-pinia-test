c2ff3deb292ecf8bae153ba7f9b0f84a2fab54d1
Dalinte <kollekcionerssd1@gmail.com>
2024-03-28 12:50:21 +0500

    add prettier, eslint adn configs. fix import vue component in ts problem

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,13 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-
 const vuetify = createVuetify({
-    components,
-    directives,
+  components,
+  directives,
 })
 
 // Pinia
 import { createPinia } from 'pinia'
 const pinia = createPinia()
 
-createApp(App)
-    .use(vuetify)
-    .use(pinia)
-    .mount('#app')
+createApp(App).use(vuetify).use(pinia).mount('#app')
