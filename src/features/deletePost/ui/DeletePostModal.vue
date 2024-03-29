9db36fecc8063e378331e847ebe7f7f4737e614c
Dalinte <kollekcionerssd1@gmail.com>
2024-03-29 15:50:06 +0500

    refs delete-modal styles

--- src/features/deletePost/ui/DeletePostModal.vue
+++ src/features/deletePost/ui/DeletePostModal.vue
@@ -18,8 +18,8 @@ const handlerDeleteClick = () => {
 
 <template>
   <div class="text-center pa-4">
-    <v-dialog v-model="isShow" max-width="400" persistent>
-      <template v-slot:activator="{ props: activatorProps }">
+    <v-dialog v-model="isShow" max-width="400">
+      <template #activator="{ props: activatorProps }">
         <v-btn
           v-bind="activatorProps"
           icon="$delete"
@@ -29,27 +29,22 @@ const handlerDeleteClick = () => {
       </template>
 
       <v-card>
-        <v-card-title class="d-flex justify-space-between align-center">
-          <div class="text-h5 text-medium-emphasis ps-2">
-            Подтверждение действия
-          </div>
-          <v-btn
-            icon="mdi-close"
-            variant="text"
-            @click="isShow = false"
-          ></v-btn>
-        </v-card-title>
-        <v-divider class="mb-2"></v-divider>
-        <v-card-text>
-          <div class="text-medium-emphasis mb-4">
+        <template #title>
+          <div class="text-h5 text-medium-emphasis">Подтверждение действия</div>
+        </template>
+        <v-divider></v-divider>
+        <template #text>
+          <div class="text-medium-emphasis mb-4 mt-2">
             Вы действительно хотите удалить пост №{{ props.id }}?
           </div>
-        </v-card-text>
+        </template>
 
-        <template v-slot:actions>
+        <template #actions>
           <v-spacer></v-spacer>
-          <v-btn @click="isShow = false"> Отменить </v-btn>
-          <v-btn color="red" @click="handlerDeleteClick"> Удалить </v-btn>
+          <v-btn @click="isShow = false">Отменить</v-btn>
+          <v-btn color="red" variant="tonal" @click="handlerDeleteClick">
+            Удалить
+          </v-btn>
         </template>
       </v-card>
     </v-dialog>
